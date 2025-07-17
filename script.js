5950c8ef8d2e02b717f795a569d644daa3eebe6c
Uali4494 <46102821+Uali4494@users.noreply.github.com>
2025-07-17 05:59:23 +0500

    Updated Project section

--- script.js
+++ script.js
@@ -15,3 +15,47 @@ document.addEventListener("DOMContentLoaded", () => {
     appearOnScroll.observe(fader);
   });
 });
+
+
+const imageSets = {
+  robot: ["images/robot1.jpg", "images/robot2.jpg", "images/robot3.jpg"],
+  curtain: ["images/curtain1.jpg", "images/curtain2.jpg"]
+};
+
+let currentIndex = {
+  robot: 0,
+  curtain: 0
+};
+
+
+function openLightbox(id) {
+  document.getElementById(id).style.display = 'flex';
+}
+
+function closeLightbox(id) {
+  document.getElementById(id).style.display = 'none';
+}
+
+function showImage(containerId, imgSrc, thumb) {
+  const mainImg = document.querySelector(`#${containerId} .lightbox-main-img`);
+  const thumbs = document.querySelectorAll(`#${containerId} .lightbox-thumbs img`);
+  mainImg.src = imgSrc;
+  thumbs.forEach(img => img.classList.remove('active'));
+  if (thumb) thumb.classList.add('active');
+}
+
+
+
+
+
+function nextImage(key) {
+  currentIndex[key]++;
+  if (currentIndex[key] >= imageSets[key].length) currentIndex[key] = 0;
+  document.getElementById(`${key}-image`).src = imageSets[key][currentIndex[key]];
+}
+
+function prevImage(key) {
+  currentIndex[key]--;
+  if (currentIndex[key] < 0) currentIndex[key] = imageSets[key].length - 1;
+  document.getElementById(`${key}-image`).src = imageSets[key][currentIndex[key]];
+}
